125a39fe4d51c852eb2bcfb72f80c30301fb8902
RaviSankarSV <ravisankarsv5577@gmail.com>
2026-02-21 15:28:10 +0530

    added request body from the pastman

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const connectDB = require('../configuration/database');
-const app = express();
 const User = require('../models/user');
 
-app.post("/signup", async (req, res) => {
-    const userObj = {
-        firstName: "Siva",
-        lastName: "Sankar",
-        emailId: "sivasankaremail@gmail.com",
-        password: "sankarpassword555",
-        age: 38,
-        gender: "male"
-    }
+const app = express();
+app.use(express.json());
 
-    //craating a new instance of User Object and inserting into model User
-    const user2 = new User(userObj);
-    await user2.save();
+app.post("/signup", async (req, res) => {
+    
+    console.log("req", req.body);
+
+    // const userObj = {
+    //     firstName: "MS",
+    //     lastName: "Dhoni",
+    //     emailId: "dhoniemail@gmail.com",
+    //     password: "sankarpassword555",
+    //     age: 38,
+    //     gender: "male"
+    // }
+
+    // //craating a new instance of User Object and inserting into model User
+    const user = new User(req.body);
+    await user.save();
     res.send("User created successfully");
 })
 
